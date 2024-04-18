6003026a90926ad6d3a8e68ef1334cc78ab3dce1
Sarah Ridley <sarah_ridley@brown.edu>
2024-04-18 10:55:02 -0400

    replacing pcr with new firebase_token function

--- server/src/context.rs
+++ server/src/context.rs
@@ -1,12 +1,12 @@
 use crate::config::Config;
 use alohomora::context::Context;
 use alohomora::db::{BBoxConn, BBoxOpts};
-use alohomora::pcr::PrivacyCriticalRegion;
+use alohomora::pure::PrivacyPureRegion;
 use alohomora::rocket::{BBoxRequest, BBoxRequestOutcome, FromBBoxRequest};
 use alohomora::AlohomoraType;
 use alohomora::{bbox::BBox, policy::NoPolicy};
 use rocket::State;
-use rocket_firebase_auth::{BearerToken, FirebaseAuth};
+use rocket_firebase_auth::{BearerToken, FirebaseAuth, FirebaseToken};
 use std::boxed::Box;
 use std::convert::TryFrom;
 use std::{sync::Arc, sync::Mutex};
@@ -41,16 +41,6 @@ impl Clone for ContextDataType {
     }
 }
 
-async fn firebase_auth_helper(token: String, firebase_auth: &FirebaseAuth) -> Option<String> {
-    match BearerToken::try_from(token.as_str()) {
-        Err(_) => None,
-        Ok(token) => match firebase_auth.verify(token.as_str()).await {
-            Err(_) => None,
-            Ok(token) => Some(token.email.unwrap()),
-        },
-    }
-}
-
 // Build the custom payload for the context given HTTP request.
 #[rocket::async_trait]
 impl<'a, 'r> FromBBoxRequest<'a, 'r> for ContextDataType {
@@ -75,18 +65,15 @@ impl<'a, 'r> FromBBoxRequest<'a, 'r> for ContextDataType {
         db.query_drop("USE users").unwrap();
 
         // Get token from headers
-        let token = request
-            .headers()
-            .get_one::<NoPolicy>("Authorization")
-            .map(|token| {
-                token.into_unbox(Context::<ContextDataType>::empty(), PrivacyCriticalRegion::new(|token, _| {
-                    firebase_auth_helper(token, &firebase_auth)
-                }), ()).unwrap()
-            });
+        let token = request.firebase_token(&firebase_auth).await;
 
         let user = match token {
             None => None,
-            Some(bbox) => BBox::new(bbox.await, NoPolicy::new()).transpose(),
+            Some(token_bbox) => {
+                Some(token_bbox.into_ppr(PrivacyPureRegion::new(|token: FirebaseToken| {
+                    token.email.unwrap()
+                })))
+            },
         };
 
         // Return resulting context.
