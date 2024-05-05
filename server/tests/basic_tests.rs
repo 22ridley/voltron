0ad1113e3833d4b77880ad475eda7a6bdc1876fc
Sarah Ridley <sarah_ridley@brown.edu>
2024-05-05 16:34:54 -0400

    test_login works

--- server/tests/basic_tests.rs
+++ server/tests/basic_tests.rs
@@ -2,6 +2,7 @@ use alohomora::testing::BBoxClient;
 use rocket::http::{ContentType, Header, Status};
 use rocket::response::{Response, Body};
 use std::thread;
+use std::io::Read;
 
 use voltron::build_server_jwk;
 mod common;
@@ -13,19 +14,27 @@ use rocket_firebase_auth::{
     FirebaseAuth,
 };
 
-use serde::{Deserialize, Serialize};
+use rocket::serde::{Deserialize, Serialize};
 use serde_json::json;
 use wiremock::{
     matchers::{method, path},
     Mock, MockServer, ResponseTemplate,
 };
 
+#[derive(Deserialize)]
+struct LoginResponse {
+    email: String,
+    name: String,
+    privilege: i32,
+    success: bool
+}
+
 // RUN WITH RUST_TEST_THREADS=1 cargo test
 
 #[tokio::test]
-async fn test_connect() {
+async fn test_login() {
     let mock_jwk_server = setup_mock_server().await;
-    let scenario = SCENARIO_HAPPY_PATH.clone();
+    let scenario = SCENARIO.clone();
     let jwk = Jwk::new(scenario.kid.as_str(), scenario.jwk_n.as_str());
 
     mock_jwk_issuer(vec![jwk].as_slice())
@@ -54,18 +63,25 @@ async fn test_connect() {
 
         // check to make sure we can connect
         let value = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc2MDI3MTI2ODJkZjk5Y2ZiODkxYWEwMzdkNzNiY2M2YTM5NzAwODQiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU2FyYWggUmlkbGV5IiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0t3enFCMzZGMS1WWWZyT2REUnp0X3JlZjJ0ZENSRlBHa2hKSlNfa1FMQj1zOTYtYyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS92b2x0cm9uLTFlYTVjIiwiYXVkIjoidm9sdHJvbi0xZWE1YyIsImF1dGhfdGltZSI6MTcxNDYxMjYzMywidXNlcl9pZCI6IlpZR29ndnd1bnRNWUp4bGdvelNXdWs4RkNmQzIiLCJzdWIiOiJaWUdvZ3Z3dW50TVlKeGxnb3pTV3VrOEZDZkMyIiwiaWF0IjoxNzE0NzUyMjE4LCJleHAiOjE3MTQ3NTU4MTgsImVtYWlsIjoic2FyYWhfcmlkbGV5QGJyb3duLmVkdSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTA1NzI4MzU3MzEzMjA1OTk4ODU2Il0sImVtYWlsIjpbInNhcmFoX3JpZGxleUBicm93bi5lZHUiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.XwxlXdkvcdVstZ3O_DXoDv4_03m1CBOCV2Uutq-SpAZOY2iEsf_yCuVPagbgt8Pu-oIMfJTf5aLnjktyHefhVr6YCeD0xTo8xF-yPSNrwKDI1uf_ftQCdhuS5fLLynH9I74aE5tM8nfXY20_iUbO3a-XHFQRwovlMYwGRhT2X461m-nnlGHeVhxcnG5l1hg1vnm8E8ZEkttrOGq49TDzzpbourOxgnZqSkzIjrT-a-rXy02LDep_E7o92J9anMbIk_rorJ29q0tNAAJg4QhljPA5O_EcGITqVS1rxVYM-ufhIAJDy0AoCkUmDe_qSH_USJ5VLaaQf3pTfz6vzY-Fbw";
-        let value = "Bearer ".to_owned() + SCENARIO_HAPPY_PATH.clone().token.as_str();
+        let value = "Bearer ".to_owned() + SCENARIO.clone().token.as_str();
         let header = Header::new("Authorization", value);
         let mut request = client.get("/login");
         request.add_header(header);
         let response = request.dispatch();
         assert!(response.status() == Status::Ok);
+
+        // We got the correct user!
+        let response_body: LoginResponse = response.into_json::<LoginResponse>().unwrap();
+        assert_eq!(response_body.email, "sarah_ridley@brown.edu");
+        assert_eq!(response_body.name, "Prof. S");
+        assert_eq!(response_body.privilege, 1);
+        assert_eq!(response_body.success, true);
     }).join().expect("Thread panicked")
 }
 
 // HELPERS
 
-static SCENARIO_HAPPY_PATH: Lazy<Scenario> = Lazy::new(|| {
+static SCENARIO: Lazy<Scenario> = Lazy::new(|| {
     Scenario {
         //token: "eyJhbGciOiJSUzI1NiIsImtpZCI6InRlc3Rfa2lkIn0.eyJzdWIiOiJzb21lLXVpZCIsImF1ZCI6ImR1bW15LXByb2plY3QtaWQiLCJpYXQiOjE2NjcwOTc2MjMsImV4cCI6NTAwNzA5OTYyMywiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2R1bW15LXByb2plY3QtaWQifQ.A0ljz9xFb9p2CC6MzOlm-In-1wLIbbZVx3YgSTeajxEIYdrqNBVp-wh00mfwPM9ZGtuPmOUP8wbEzY_XjClNFCWdExmRY-UTLga0IeAekDgo8uWgr4g3cVQ5-RSK-mSDl1svzaZBq2YYyWpYZkfcsp2eoJhS04A4t9d_ADnvzNTC9YlPAyDp6DeDvyUE9K6B6_xIj2murScOz9CKijJq1rzItl1Ip8JVlNrEVFeiF5a7MZTMZYkV6ZN31hOCqc1N4eIhxHJ3ZvvM6YbZva-pdFsu_XGroYIb4Ar_hDpa3i4KrM517mp9e_nocGFKkjfQpNUnp0lsJ_PzRZZvHZ5rrQ".to_string(),
         token: "eyJhbGciOiJSUzI1NiIsImtpZCI6InRlc3Rfa2lkIn0.eyJzdWIiOiJzb21lLXVpZCIsImF1ZCI6ImR1bW15LXByb2plY3QtaWQiLCJpYXQiOjE2NjcwOTc2MjMsImV4cCI6NTAwNzA5OTYyMywiZW1haWwiOiJzYXJhaF9yaWRsZXlAYnJvd24uZWR1IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2R1bW15LXByb2plY3QtaWQifQ.RCRV9iT6245UMQEJ8App7rkMHqicLDkPC4ENgy3wxPECLRltpNfA6PADUZ_krxqcKto93thJjjAtujIAj7BXIhADOt2WPGCWcG1pC1zIZxNZ3mPBNQ62WuZNn7aoUAOxU6YuFHLXQP99goZmyw3fAX5N2PP5TWKfl8XoVAL2bl_s8WG5ogqJCCVfx3zM4fzJSbGuIphL51r8DjF_mPmfBs-EoBC1N8z8xZXKyVn0gR0aZM_lWb2ClJ-RNvW5yNdvlr_SwjUEXW6iZ1UL5tq-C-_Vae0hNzTaey-Uu98dEQk6STOR23ZZWtEwCzIr6hCUn1xkKpvdgmwPSP7JNqsllg".to_string(),
