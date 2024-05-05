659884580119379533244b084000f3d2167a2707
Sarah Ridley <sarah_ridley@brown.edu>
2024-05-05 19:36:24 -0400

    test_stud passes

--- server/tests/basic_tests.rs
+++ server/tests/basic_tests.rs
@@ -1,25 +1,11 @@
 use alohomora::testing::BBoxClient;
-use rocket::http::{ContentType, Header, Status};
-use rocket::response::{Response, Body};
+use rocket::http::{Header, Status};
 use std::thread;
-use std::io::Read;
-
 use voltron::build_server_test;
 mod common;
-use common::{JWK_N, KID, INSTR_TOKEN, TEST_JWKS_URL, setup_mock_server, mock_jwk_issuer};
-use once_cell::sync::Lazy;
-use rocket_firebase_auth::{
-    errors::{Error, InvalidJwt},
-    jwk::Jwk,
-    FirebaseAuth,
-};
-
-use rocket::serde::{Deserialize, Serialize};
-use serde_json::json;
-use wiremock::{
-    matchers::{method, path},
-    Mock, MockServer, ResponseTemplate,
-};
+use common::{JWK_N, KID, INSTR_TOKEN, STUD_TOKEN, ADMIN_TOKEN, setup_mock_server, mock_jwk_issuer};
+use rocket_firebase_auth::jwk::Jwk;
+use rocket::serde::Deserialize;
 
 #[derive(Deserialize)]
 struct LoginResponse {
@@ -53,6 +39,14 @@ struct StudentGroup {
 struct SuccessResponse {
     pub success: bool,
     pub message: String
+}
+
+#[derive(Deserialize)]
+pub struct StudentResponse {
+    pub success: bool,
+    pub class_id: i64,
+    pub group_id: i64,
+    pub contents: Option<String>
 }
 
 // RUN WITH RUST_TEST_THREADS=1 cargo test
@@ -103,11 +97,64 @@ struct SuccessResponse {
 //     }).join().expect("Thread panicked")
 // }
 
+// #[tokio::test]
+// async fn test_instr() {
+//     // TEST ACTIONS BY INSTRUCTORS: 
+//     // - Viewing student buffers
+//     // - Registering students
+
+//     let mock_jwk_server = setup_mock_server().await;
+//     let jwk = Jwk::new(KID, JWK_N);
+
+//     mock_jwk_issuer(vec![jwk].as_slice())
+//         .mount(&mock_jwk_server)
+//         .await;
+
+//     // Send a request
+//     let server = build_server_test();
+//     thread::spawn(|| {
+//         let client: BBoxClient = BBoxClient::tracked(server).unwrap();
+
+//         // Check to make sure we can see student buffers
+//         let value = "Bearer ".to_owned() + INSTR_TOKEN.clone();
+//         let header = Header::new("Authorization", value);
+//         let mut instr_request = client.get("/instructor");
+//         instr_request.add_header(header.clone());
+//         let instr_response = instr_request.dispatch();
+//         assert!(instr_response.status() == Status::Ok);
+
+//         let mut instr_response_body: InstructorResponse = instr_response.into_json::<InstructorResponse>().unwrap();
+//         let current_num_students = instr_response_body.students.len();
+//         assert!(0 < current_num_students);
+//         assert_eq!(instr_response_body.class_id, 0);
+//         assert!(0 < instr_response_body.student_groups.len());
+
+//         // Check to make sure that we can register students
+//         let mut reg_request = client.post("/register_student?stud_group=0&stud_name=paul&stud_class=0&stud_email=paul@gmail.com");
+//         reg_request.add_header(header.clone());
+//         let reg_response = reg_request.dispatch();
+//         assert!(reg_response.status() == Status::Ok);
+        
+//         let reg_response_body: SuccessResponse = reg_response.into_json::<SuccessResponse>().unwrap();
+//         assert_eq!(reg_response_body.success, true);
+
+//         // Check to make sure that the student was registered (we have 1 more student!)
+//         let mut new_instr_request = client.get("/instructor");
+//         new_instr_request.add_header(header.clone());
+//         let new_instr_response = new_instr_request.dispatch();
+//         assert!(new_instr_response.status() == Status::Ok);
+
+//         let new_instr_response_body = new_instr_response.into_json::<InstructorResponse>().unwrap();
+//         assert_eq!(new_instr_response_body.students.len(), current_num_students + 1);
+
+//     }).join().expect("Thread panicked")
+// }
+
 #[tokio::test]
-async fn test_instr() {
-    // TEST ACTIONS BY INSTRUCTORS: 
-    // - Viewing student buffers
-    // - Registering students
+async fn test_stud() {
+    // TEST ACTIONS BY STUDENTS: 
+    // - Viewing their own buffers
+    // - Writing to their own buffer
 
     let mock_jwk_server = setup_mock_server().await;
     let jwk = Jwk::new(KID, JWK_N);
@@ -122,45 +169,37 @@ async fn test_instr() {
         let client: BBoxClient = BBoxClient::tracked(server).unwrap();
 
         // Check to make sure we can see student buffers
-        let value = "Bearer ".to_owned() + INSTR_TOKEN.clone();
+        let value = "Bearer ".to_owned() + STUD_TOKEN;
         let header = Header::new("Authorization", value);
-        let mut instr_request = client.get("/instructor");
-        instr_request.add_header(header.clone());
-        let instr_response = instr_request.dispatch();
-        assert!(instr_response.status() == Status::Ok);
+        let mut stud_request = client.get("/student");
+        stud_request.add_header(header.clone());
+        let stud_response = stud_request.dispatch();
+        assert!(stud_response.status() == Status::Ok);
 
-        let mut instr_response_body: InstructorResponse = instr_response.into_json::<InstructorResponse>().unwrap();
-        let current_num_students = instr_response_body.students.len();
-        assert!(0 < current_num_students);
-        assert_eq!(instr_response_body.class_id, 0);
-        assert!(0 < instr_response_body.student_groups.len());
+        let stud_response_body: StudentResponse = stud_response.into_json::<StudentResponse>().unwrap();
+        assert_eq!(stud_response_body.class_id, 0);
+        assert_eq!(stud_response_body.group_id, 0);
+        assert!(stud_response_body.contents.is_some());
 
-        // Check to make sure that we can register students
-        let mut reg_request = client.post("/register_student?stud_group=0&stud_name=paul&stud_class=0&stud_email=paul@gmail.com");
-        reg_request.add_header(header.clone());
-        let reg_response = reg_request.dispatch();
-        assert!(reg_response.status() == Status::Ok);
+        // Check to make sure we can write to our buffer
+        let mut upd_request = client.post("/update?text=//%20This%20is%20a%20comment!");
+        upd_request.add_header(header.clone());
+        let upd_response = upd_request.dispatch();
+        assert!(upd_response.status() == Status::Ok);
         
-        let reg_response_body: SuccessResponse = reg_response.into_json::<SuccessResponse>().unwrap();
-        assert_eq!(reg_response_body.success, true);
+        let upd_response_body: SuccessResponse = upd_response.into_json::<SuccessResponse>().unwrap();
+        assert_eq!(upd_response_body.success, true);
 
-        // Check to make sure that the student was registered (we have 1 more student!)
-        let mut new_instr_request = client.get("/instructor");
-        new_instr_request.add_header(header.clone());
-        let new_instr_response = new_instr_request.dispatch();
-        assert!(new_instr_response.status() == Status::Ok);
+        // Check to make sure that buffer contents are updated
+        let mut new_stud_request = client.get("/student");
+        new_stud_request.add_header(header.clone());
+        let new_stud_response = new_stud_request.dispatch();
+        assert!(new_stud_response.status() == Status::Ok);
 
-        let new_instr_response_body = new_instr_response.into_json::<InstructorResponse>().unwrap();
-        assert_eq!(new_instr_response_body.students.len(), current_num_students + 1);
+        let new_stud_response_body: StudentResponse = new_stud_response.into_json::<StudentResponse>().unwrap();
+        assert_eq!(new_stud_response_body.contents.unwrap(), "// This is a comment!");
 
     }).join().expect("Thread panicked")
-}
-
-#[tokio::test]
-async fn test_stud() {
-    // TEST ACTIONS BY STUDENTS: 
-    // - Viewing their own buffers
-    // - Writing to their own buffer
 }
 
 #[tokio::test]
--- server/tests/common.rs
+++ server/tests/common.rs
@@ -1,16 +1,4 @@
-use alohomora::testing::BBoxClient;
-use rocket::http::{ContentType, Header, Status};
-use rocket::response::{Response, Body};
-use std::thread;
-use std::io::Read;
-
-use once_cell::sync::Lazy;
-use rocket_firebase_auth::{
-    errors::{Error, InvalidJwt},
-    jwk::Jwk,
-    FirebaseAuth,
-};
-
+use rocket_firebase_auth::jwk::Jwk;
 use rocket::serde::{Deserialize, Serialize};
 use serde_json::json;
 use wiremock::{
@@ -19,13 +7,12 @@ use wiremock::{
 };
 
 pub static TEST_JWKS_URL: &str = "http://localhost:8888/jwks_url";
+pub static JWK_N: &str = "XiqTPp559nQUm1dxEecu1HT_yD0Vv4OY9uVG-YNCy8NU2ceaqoUfF05luJ9xtmf-D3PQjpwSouA2ejxr_GSD2VQ99our7CDe5t5jdNEXyXSrXJDua5-lEETT-fOmnAuhti9m86w7Jd2HafJCq9sGEuQc4-CM1-2faerEGGl9iyNGQEZBnS_ExDJOJnbI8J6JZnWhL_rjwkVcM6_MX9bDA1Hi4fosMW1V-WhnZUJ5-WvNfX_2feXfQ0qq5kH9BWhbYCxwwT2d_0xkdL0aItEB_nqZSS9yIDiGQATjf_P708dEo9A0tVZg9HUtLY5k4-JCrOUayJOYJ5b-YGETzSbELw";
+pub static KID: &str = "test_kid";
 
 pub static INSTR_TOKEN: &str = "eyJhbGciOiJSUzI1NiIsImtpZCI6InRlc3Rfa2lkIn0.eyJzdWIiOiJzb21lLXVpZCIsImF1ZCI6ImR1bW15LXByb2plY3QtaWQiLCJpYXQiOjE2NjcwOTc2MjMsImV4cCI6NTAwNzA5OTYyMywiZW1haWwiOiJzYXJhaF9yaWRsZXlAYnJvd24uZWR1IiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2R1bW15LXByb2plY3QtaWQifQ.RCRV9iT6245UMQEJ8App7rkMHqicLDkPC4ENgy3wxPECLRltpNfA6PADUZ_krxqcKto93thJjjAtujIAj7BXIhADOt2WPGCWcG1pC1zIZxNZ3mPBNQ62WuZNn7aoUAOxU6YuFHLXQP99goZmyw3fAX5N2PP5TWKfl8XoVAL2bl_s8WG5ogqJCCVfx3zM4fzJSbGuIphL51r8DjF_mPmfBs-EoBC1N8z8xZXKyVn0gR0aZM_lWb2ClJ-RNvW5yNdvlr_SwjUEXW6iZ1UL5tq-C-_Vae0hNzTaey-Uu98dEQk6STOR23ZZWtEwCzIr6hCUn1xkKpvdgmwPSP7JNqsllg";
 pub static STUD_TOKEN: &str = "eyJhbGciOiJSUzI1NiIsImtpZCI6InRlc3Rfa2lkIn0.eyJzdWIiOiJzb21lLXVpZCIsImF1ZCI6ImR1bW15LXByb2plY3QtaWQiLCJpYXQiOjE2NjcwOTc2MjMsImV4cCI6NTAwNzA5OTYyMywiZW1haWwiOiJzYXJhaC5rYXRlLnJpZGxleUBnbWFpbC5jb20iLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZHVtbXktcHJvamVjdC1pZCJ9.Sos5_qeTSxsCdotSOHdsldlTc5Y8WEZGcPl1ZTsM423NTbBE-DjF3jO6iveyetbYQSylwQ3toAXmkEvjU0zmLek7kDSvsdhKIpu8ccqv92YpH3sk1fdVspitdwnkxdQ5IYRy_Yoj0OTVPvtwd6_JrUiCVuTbMAsjvYzYOTT3oC9NNkYVmSOYMnh3UmPGggfLSHgJN2d53HjSCmrCOjV-Sqmua6Zr4wh8btDWkM19vVL1yYnjlNu9OLbjkCtVbc-Em0DM6LefgFIp0qxmWBJMwIK1FlqDv5uHZ0eeGY9HWYeREys-owBiwP1OyJmWIPrSZ0UpOlrwx2kSJC5ragTwmA";
 pub static ADMIN_TOKEN: &str = "eyJhbGciOiJSUzI1NiIsImtpZCI6InRlc3Rfa2lkIn0.eyJzdWIiOiJzb21lLXVpZCIsImF1ZCI6ImR1bW15LXByb2plY3QtaWQiLCJpYXQiOjE2NjcwOTc2MjMsImV4cCI6NTAwNzA5OTYyMywiZW1haWwiOiIyMnJpZGxleXNrQGdtYWlsLmNvbSIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9kdW1teS1wcm9qZWN0LWlkIn0.K-MAV5ds_rviiIB3AMENhyzBaacSl45PiOP6UWQk6v9A6x7bbRv3H7rqA5NxDYJBN_sOZfYZDlucaQfcNC_tDvxkwmq_Q86VEl-sLlFGBlkumjAg_dL7NMpYcq-8PmTDR6PBwpjHyRcj1uXHQvop8LGkDGaWoUAfzHXli0fPH0jBo5x4FfLxmtDK7m7rBStXz1wGaxNv7Gx8kVF51zsKe9bJDMoF7RBdPW7iiaQWqyR7vd3L1u9PI9U1al7rzB89LMQIt4hxYs8Dc5VqqcVp7LVS2xZ3DXFi6H1GdFfw1A6ONEfG9xv-uLqXThIKwQ6UWUATjlSU9ndn1qTMrgBWzQ";
-
-pub static JWK_N: &str = "XiqTPp559nQUm1dxEecu1HT_yD0Vv4OY9uVG-YNCy8NU2ceaqoUfF05luJ9xtmf-D3PQjpwSouA2ejxr_GSD2VQ99our7CDe5t5jdNEXyXSrXJDua5-lEETT-fOmnAuhti9m86w7Jd2HafJCq9sGEuQc4-CM1-2faerEGGl9iyNGQEZBnS_ExDJOJnbI8J6JZnWhL_rjwkVcM6_MX9bDA1Hi4fosMW1V-WhnZUJ5-WvNfX_2feXfQ0qq5kH9BWhbYCxwwT2d_0xkdL0aItEB_nqZSS9yIDiGQATjf_P708dEo9A0tVZg9HUtLY5k4-JCrOUayJOYJ5b-YGETzSbELw";
-pub static KID: &str = "test_kid";
 
 pub async fn setup_mock_server() -> MockServer {
     let listener = std::net::TcpListener::bind("localhost:8888").unwrap();
